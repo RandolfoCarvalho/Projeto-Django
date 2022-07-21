8000a203496928065e7e1287463abd6402e33abb
RandolfoCarvalho <randolfoirades@gmail.com>
2022-07-20 22:40:41 -0300

    treino

---
+++ receitas/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'receitas/home.html', context={
+        'name': 'randolfo carvalho'
+    })
+
+
+def contato(request):
+    return HttpResponse('contato1')
+
+
+def sobre(request):
+    return HttpResponse('sobre')
+
+
