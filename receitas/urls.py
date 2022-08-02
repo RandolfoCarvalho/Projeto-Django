1856751d299f333caba0f704c97368c8946033c7
RandolfoCarvalho <randolfoirades@gmail.com>
2022-08-01 22:20:53 -0300

    Removido contato e sobre

--- receitas/urls.py
+++ receitas/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from receitas.views import contato, home, sobre
+from receitas.views import home
 
 
 urlpatterns = [
     path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
 
 ]
