9444c0851cb6cf530440e91e6836c829a00c514b
RandolfoCarvalho <randolfoirades@gmail.com>
2022-08-01 22:12:10 -0300

    Nova Versão

--- receitas/urls.py
+++ receitas/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from receitas.views import home
+from receitas.views import contato, home, sobre
+
 
 urlpatterns = [
     path('', home),
-    
+    path('sobre/', sobre),
+    path('contato/', contato),
+
 ]
--- receitas/views.py
+++ receitas/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-# Create your views here.
 
 
 def home(request):
-    return render(request, 'receitas/pages/home.html',)
+    return render(request, 'receitas/home.html', {
+        'name': "randolfo"
+    }) 
+
+    
+def contato(request):
+    return HttpResponse('contato')
 
 
-
-
+def sobre(request):
+    return HttpResponse('sobre')
